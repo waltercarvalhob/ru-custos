aaff03608762b9c99be75123a83e79a9a53b0e61
Walter Carvalho Barbosa <waltercarvalhob@gmail.com>
2026-08-24 10:21:39 -0300

    Adiciona blueprint e configuracao para deploy no Render.com

--- backend/gateway/src/main/java/br/edu/ufma/rucustos/gateway/config/CorsConfig.java
+++ backend/gateway/src/main/java/br/edu/ufma/rucustos/gateway/config/CorsConfig.java
@@ -20,7 +20,8 @@ public class CorsConfig implements WebMvcConfigurer {
                         "http://172.16.*.*:*", "http://172.17.*.*:*", "http://172.18.*.*:*", "http://172.19.*.*:*",
                         "http://172.20.*.*:*", "http://172.21.*.*:*", "http://172.22.*.*:*", "http://172.23.*.*:*",
                         "http://172.24.*.*:*", "http://172.25.*.*:*", "http://172.26.*.*:*", "http://172.27.*.*:*",
-                        "http://172.28.*.*:*", "http://172.29.*.*:*", "http://172.30.*.*:*", "http://172.31.*.*:*"
+                        "http://172.28.*.*:*", "http://172.29.*.*:*", "http://172.30.*.*:*", "http://172.31.*.*:*",
+                        "https://*.onrender.com"
                 )
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
